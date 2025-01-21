26e13c2fba4b66c9233b8e6869ccbd9281924a7c
Sankeethan01 <sankee26july@gmail.com>
2025-01-21 22:46:19 +0530

    updated now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative shadow-2xl'>
 
             {/*Mobile screen*/}
-            <div className='h-full flex items-center justify-between md:hidden'>
+            <div className='h-full flex items-center justify-between w-full md:hidden'>
                 <Link href='/'>
                     <div className='text-2xl tracking-wide'>StarLight</div>
                 </Link>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,7 @@ const SearchBar = () => {
     }
 
   return (
-    <form className="flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1/2 md:flex-1" onSubmit={handleSearch}>
+    <form className="flex items-center justify-between gap-3 w-1/3 md:w-full bg-gray-100 p-2 rounded-md flex-1/2 md:flex-1" onSubmit={handleSearch}>
         <input type="text" name='search-text' placeholder="Search here" className='flex-1 bg-transparent outline-none'/>
         <button className='cursor-pointer' type='submit'>
           <Image src={searchImg} alt='search image' width={16} height={16}/>
